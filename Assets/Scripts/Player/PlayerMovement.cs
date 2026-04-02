dd22dac9e7e30f5d2815a9caff49d31dc6ca196f
mithrank <mithuk7b@gmail.com>
2026-04-03 00:23:38 +0530

    Character movement animations + flip

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] private float gameSpeed = 5f;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 movement;
+    private Animator anim;
+    private bool isFacingRight = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        FlipCharacterX();
+    }
+
+    private void HandleMovement()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        movement.x = input * gameSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+        if(input > 0) {
+            anim.SetBool("isRunning", true);
+            isFacingRight = true;
+        } else if(input < 0) {
+            anim.SetBool("isRunning", true);
+            isFacingRight = false;
+        } else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+    private void FlipCharacterX()
+    {
+        if(isFacingRight)
+        {
+            spriteRenderer.flipX = false;
+        } else
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
