f3be1b38e1c8c922f356e89ee77a2c96fd43b3b3
mithrank <mithuk7b@gmail.com>
2026-04-05 00:32:37 +0530

    Fixed infinite jump + 3 raycast check

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -6,24 +6,49 @@ public class PlayerJump : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sr;
     [SerializeField] private float jumpForce = 6f;
+    private float playerHalfHeight;
+    private float halfWidth;
+    // private bool isGrounded;
     void Start()
     {
-        return;
+        playerHalfHeight = sr.bounds.extents.y + 0.1f;
+        halfWidth = sr.bounds.extents.x * 0.8f;
+        // Debug.Log(playerHalfHeight);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Jump"))
+
+        if(Input.GetButtonDown("Jump") && GetIsGrounded())
         {
             Jump();
         }
     }
 
+    private bool GetIsGrounded()
+    {
+
+        Vector2 center = transform.position;
+        Vector2 left = center + Vector2.left * halfWidth;
+        Vector2 right = center + Vector2.right * halfWidth;
+
+        bool hitCenter = Physics2D.Raycast(center, Vector2.down, playerHalfHeight, LayerMask.GetMask("Ground"));
+        bool hitLeft = Physics2D.Raycast(left, Vector2.down, playerHalfHeight, LayerMask.GetMask("Ground"));
+        bool hitRight = Physics2D.Raycast(right, Vector2.down, playerHalfHeight, LayerMask.GetMask("Ground"));
+
+        Debug.DrawRay(center, Vector2.down * playerHalfHeight, Color.red);
+        Debug.DrawRay(left, Vector2.down * playerHalfHeight, Color.red);
+        Debug.DrawRay(right, Vector2.down * playerHalfHeight, Color.red);
+
+        return hitCenter || hitLeft || hitRight;
+    }
+
     private void Jump()
     {
-        rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
